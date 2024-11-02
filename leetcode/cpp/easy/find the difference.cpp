6536b40e050e593e878025292db0b50d448dbe06
Satyam  Kumar Jaiswal <94980142+1satyam@users.noreply.github.com>
2024-11-02 16:57:33 +0530

    bit manipulation

---
+++ leetcode/cpp/easy/find the difference.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int res=0;
+        for(int i=0;i<s.size();i++)
+        {
+            res^=s[i];
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            res^=t[i];
+        }
+        return char(res);     
+    }
+};
